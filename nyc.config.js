b2525945ee4f895e5743935ccfe843ba56eb6e09
B. Mearns <mearns@users.noreply.github.com>
2019-07-02 18:47:13 +0000

    Initial commit

---
+++ nyc.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    include: ["src/**/*.js", "test/test-utils/**/*.js"],
+    exclude: ["test/src/**/*"],
+    all: true,
+    reporter: ["html", "text"]
+};
---
+++ test/src/index.spec.js
@@ -0,0 +1,25 @@
+/* eslint-env mocha */
+/* eslint no-unused-expressions:0 */
+
+// Module under test
+// const moduleUnderTest = require("../../src/index");
+
+// Support
+
+// Your own custom test utilities.
+// const testUtils = require("../test-utils");
+
+// assertions library
+// const chai = require("chai");
+// const { expect } = require("chai");
+
+// mocks and stubs
+// const sinon = require("sinon");
+
+// chai plugin for assertino on sinon stuff
+// const sinonChai = require("sinon-chai");
+// chai.use(sinonChai);
+
+describe("This module", () => {
+    it("should really have some unit tests");
+});
